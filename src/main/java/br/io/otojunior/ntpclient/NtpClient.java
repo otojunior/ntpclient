39d0d2b295c478382515d3023c76101b195d7827
Oto Soares Coelho Júnior <oto.coelho-junior@serpro.gov.br>
2023-02-27 13:25:47 -0300

    Utilizando API do Windows

--- src/main/java/br/io/otojunior/ntpclient/NtpClient.java
+++ src/main/java/br/io/otojunior/ntpclient/NtpClient.java
@@ -8,8 +8,13 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalField;
 
 import org.apache.commons.net.ntp.NTPUDPClient;
+
+import com.sun.jna.platform.win32.Kernel32;
+import com.sun.jna.platform.win32.WinBase.SYSTEMTIME;
 
 /**
  * @author Oto Soares Coelho Junior
@@ -32,7 +37,6 @@ public class NtpClient {
         	final var inetAddr = InetAddress.getByName(NTP_SERVER);
         	final var fmthora = DateTimeFormatter.ofPattern("HH:mm:ss");
         	final var fmtdata = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        	final var runtime = Runtime.getRuntime();
 
         	watchStart = System.currentTimeMillis();
 
@@ -48,10 +52,21 @@ public class NtpClient {
 				.ofInstant(Instant
 				.ofEpochMilli(returnTime), defaultZoneId);
 
-			runtime.exec(new String[] {"cmd", "/C", "time", time.format(fmthora)});
-			runtime.exec(new String[] {"cmd", "/C", "date", time.format(fmtdata)});
+			var systemTime = new SYSTEMTIME();
+			systemTime.wYear = (short) time.getYear();
+	        systemTime.wMonth = (short) time.getMonth().getValue();
+	        systemTime.wDay = (short) time.getDayOfMonth();
+	        systemTime.wHour = (short) time.getHour();
+	        systemTime.wMinute = (short) time.getMinute();
+	        systemTime.wSecond = (short) time.getSecond();
+	        systemTime.wMilliseconds = (short) time.get(ChronoField.MILLI_OF_SECOND);
+	        
+	        var result = Kernel32.INSTANCE.SetLocalTime(systemTime);
+	        System.out.println(result
+	        	? "Data e hora do sistema definidas com sucesso"
+    			: "Falha ao definir a data e hora do sistema");
 
-            System.out.println("NTP time: " + time.format(fmthora));
+	        System.out.println("NTP time: " + time.format(fmthora));
             System.out.println("NTP date: " + time.format(fmtdata));
         } finally {
             client.close();
